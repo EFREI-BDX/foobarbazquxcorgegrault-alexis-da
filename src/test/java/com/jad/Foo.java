2f5bb86129fc029fbac156c4616d992142bd9727
alexis-da <alexis.dacruz40@gmail.com>
2025-02-14 18:43:55 +0100

    test

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -1,4 +1,17 @@
 package com.jad;
 
+import java.util.ArrayList;
+
 public class Foo {
+
+    private Bar bar;
+
+    public Bar getBar() {
+        return this.bar;
+    }
+
+    private ArrayList<Baz> bazs;
+    private Qux qux;
+    private Corge corge;
+    private ArrayList<Grault> graults;
 }
